36821687069fe467c23651ef20f812b42b3e52e3
ezequielW <ezequielwokraczka@gmail.com>
2021-08-05 00:31:43 -0300

    Updated app bar to responsive design

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,34 +1,79 @@
 <template>
-    <v-app-bar app color="indigo lighten-1 px-14" fade-on-scroll dark> 
-      <router-link to="/">
-        <v-img class="mx-2" :src="require('@/assets/logo.png')" max-height="40" max-width="40" contain/>
-      </router-link>
-      <v-toolbar-title>VueBlog</v-toolbar-title>
+    <div>
+      <v-app-bar color="indigo lighten-1 px-sm-14" fade-on-scroll dark> 
+        <v-app-bar-nav-icon @click="drawer = true" v-show="$vuetify.breakpoint.xsOnly"></v-app-bar-nav-icon>
+        <router-link to="/">
+          <v-img class="mx-2" :src="require('@/assets/logo.png')" max-height="40" max-width="40" contain/>
+        </router-link>
+        <v-toolbar-title>VueBlog</v-toolbar-title>
 
-      <v-divider class="mx-4" vertical/>
+        <v-divider class="mx-4" vertical v-show="$vuetify.breakpoint.smAndUp"/>
 
-      <v-toolbar-items>
-        <v-btn text to="/">Home</v-btn>
-        <v-btn text to="about">About</v-btn>
-      </v-toolbar-items>
+        <!-- Main navigation, just two pages -->
+        <v-toolbar-items v-show="$vuetify.breakpoint.smAndUp">
+          <v-btn text to="/">Home</v-btn>
+          <v-btn class="mr-auto" text to="about">About</v-btn>
+        </v-toolbar-items>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-toolbar-items v-if="!$store.state.loggedIn">
-        <v-btn text to="register">Sign up</v-btn>
-        <v-btn text to="login">Sign in</v-btn>
-      </v-toolbar-items>
-      <div v-if="$store.state.loggedIn">
-        <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
-        <v-btn icon><v-icon>mdi-account</v-icon></v-btn>
-        <v-btn @click="logout" icon><v-icon>mdi-logout</v-icon></v-btn>
-      </div>
-    </v-app-bar>
+        <!-- Sign buttons on sm plus devices -->
+        <v-toolbar-items v-if="!$store.state.loggedIn && $vuetify.breakpoint.smAndUp">
+          <v-btn text to="register">Sign in</v-btn>
+          <v-btn text to="login">Sign up</v-btn>
+        </v-toolbar-items>
+
+        <!-- Actions when already logged in -->
+        <v-toolbar-items v-if="$store.state.loggedIn">
+          <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
+          <v-btn icon><v-icon>mdi-account</v-icon></v-btn>
+          <v-btn @click="logout" icon><v-icon>mdi-logout</v-icon></v-btn>
+        </v-toolbar-items>
+        
+        <!-- Mobile only sign in/up button -->
+        <v-menu left bottom v-if="!$store.state.loggedIn && $vuetify.breakpoint.xsOnly">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item to="login">
+              <v-list-item-title>Sign in</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="register">
+              <v-list-item-title>Sign up</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+
+      <!-- Main navigation on xs devices -->
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+            <v-list-item to="/">
+              <v-list-item-icon><v-icon>mdi-home</v-icon></v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item to="about">
+              <v-list-item-icon><v-icon>mdi-information-outline</v-icon></v-list-item-icon>
+              <v-list-item-title>about</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Header',
+    data: () => ({
+      drawer: false,
+      group: null
+    }),
     methods: {
       logout() {
         this.$store.dispatch('removeToken');
